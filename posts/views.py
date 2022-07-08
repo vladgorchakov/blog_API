73104d4a15d34dd668d86d8950342b4f58b93f46
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-08 13:44:16 +0200

    Add permission IsAuthorOrReadOnly for PostDetailView

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,15 @@
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
-    #permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all().order_by('-update_at')
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    #permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
