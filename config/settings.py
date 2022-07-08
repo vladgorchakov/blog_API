cb0e953a40e60b29fa3402baabf02a27a50e3a15
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-08 13:29:22 +0200

    Add permissions

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 # Add rest_framework config
 REST_FRAMEWORK = {
         'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.AllowAny',
+            'rest_framework.permissions.IsAuthenticated',
         ]
 }
 
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,15 @@
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
 class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
+    #permission_classes = (permissions.IsAuthenticated,)
+    queryset = Post.objects.all().order_by('-update_at')
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
